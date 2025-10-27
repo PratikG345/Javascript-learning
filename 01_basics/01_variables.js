7f4c6242576b7a1e9a2f29e195db710519eb8224
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2025-10-27 14:30:53 +0530

    variables with output

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountID = 14453
+let accountEmail = "pratik@gmail.com"
+var accountPassword = "12345"
+city = "Nashik"
+let accountState
+console.table([accountID,accountEmail,accountPassword,city,accountState])
+/*
+┌─────────┬────────────────────┐
+│ (index) │ Values             │
+├─────────┼────────────────────┤
+│ 0       │ 14453              │
+│ 1       │ 'pratik@gmail.com' │
+│ 2       │ '12345'            │
+│ 3       │ 'Nashik'           │
+│ 4       │ undefined          │
+└─────────┴────────────────────┘
+*/
