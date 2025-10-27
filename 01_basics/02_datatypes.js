7df1a1ef58ebd62860c0440927743dcc975ba7c0
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2025-10-27 14:31:07 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+/* 
+number => 2 power 53
+bigint
+string => "pratik"
+boolean => true/false
+null => standalone value
+undefined => not assigned value
+symbol => uniqueness
+*/
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
